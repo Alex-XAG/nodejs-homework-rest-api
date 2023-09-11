44d373956aee3e390589585dacd9da7a9465721b
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-09-11 20:18:27 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,19 +59,23 @@ const register = async (req, res, next) => {
   }
 };
 
-const verifyEmail = async (req, res) => {
-  const { verificationToken } = req.params;
+const verifyEmail = async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
 
-  const user = await User.findOne({ verificationToken });
-  if (!user) {
-    throw HttpError(404, "User not found");
+    const user = await User.findOne({ verificationToken });
+    if (!user) {
+      throw HttpError(404, "User not found");
+    }
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: null,
+    });
+
+    res.status(200).json({ message: "Verification successful" });
+  } catch (error) {
+    next(error);
   }
-  await User.findByIdAndUpdate(user._id, {
-    verify: true,
-    verificationToken: "",
-  });
-
-  res.status(200).json({ message: "Verification successful" });
 };
 
 const resendVerifyEmail = async (req, res, next) => {
